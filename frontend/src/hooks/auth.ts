e0edfd9b765870b32c0eb67b2ec6c57dede552f3
Michael <mishatolchinski@gmail.com>
2020-12-27 15:34:15 +0200

    added k8s

--- frontend/src/hooks/auth.ts
+++ frontend/src/hooks/auth.ts
@@ -10,7 +10,8 @@ const state = reactive({
   isAuth: false
 });
 
-const authApiUrl = 'http://localhost:3000/auth';
+//const authApiUrl = 'http://localhost:3000/auth'; for compose
+const authApiUrl = '/api/auth'; //for k8s
 
 // mutations
 function setUser(payload: { token: string; userId: string; isAuth: boolean }) {
